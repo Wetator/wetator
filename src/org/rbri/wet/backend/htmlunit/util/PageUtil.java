5fb9c304c9c047230b2cb2671e936df66c1808b4
fdanek <frank.danek@gmail.com>
2010-11-04 22:21:45 +0000

    % cleanup

--- src/org/rbri/wet/backend/htmlunit/util/PageUtil.java
+++ src/org/rbri/wet/backend/htmlunit/util/PageUtil.java
@@ -38,41 +38,6 @@ import com.gargoylesoftware.htmlunit.html.XHtmlPage;
  */
 public final class PageUtil {
 
-  // /**
-  // * Wait for finishing of all jobs scheduled for execution
-  // * in the next 1s
-  // *
-  // * @param aPage the page
-  // */
-  // public static void waitForThreads(final Page aPage, long aTimeoutInSeconds) {
-  // // no wait
-  // if (aTimeoutInSeconds < 1) {
-  // // at least execute all immediate jobs
-  // JavaScriptJobManager tmpJobManager = aPage.getEnclosingWindow().getJobManager();
-  //
-  // tmpJobManager.waitForJobsStartingBefore(1000); // one second
-  // return;
-  // }
-  //
-  // // try with wait
-  // int tmpNoOfJobsLeft = 1;
-  // long tmpEndTime = System.currentTimeMillis() + aTimeoutInSeconds * 1000;
-  // while (tmpNoOfJobsLeft > 0 && System.currentTimeMillis() < tmpEndTime) {
-  // long tmpWaitTime = tmpEndTime - System.currentTimeMillis();
-  // tmpNoOfJobsLeft = tmpJobManager.waitForJobsStartingBefore(Math.max(1000, tmpWaitTime));
-  //
-  // // current page is changed, we have to make another try
-  // if (tmpHtmlPage != getCurrentHtmlPage()) {
-  // tmpNoOfJobsLeft = 1;
-  // tmpHtmlPage = getCurrentHtmlPage();
-  // tmpCurrentTitle = tmpHtmlPage.getTitleText();
-  // }
-  // }
-  //
-  // // TODO make max wait time configurable
-  //
-  // }
-
   /**
    * Helper for tests
    * 
