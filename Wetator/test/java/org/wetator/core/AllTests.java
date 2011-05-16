e4d660db30527ae2c68214fb2efd2b9cb4f0b8c7
frank.danek <frank.danek@localhost>
2011-05-16 17:26:56 +0000

    % clean up tests after renaming, moving, etc.

--- Wetator/test/java/org/wetator/core/AllTests.java
+++ Wetator/test/java/org/wetator/core/AllTests.java
@@ -26,10 +26,10 @@ import org.junit.runners.Suite.SuiteClasses;
  * @author frank.danek
  */
 @RunWith(Suite.class)
-@SuiteClasses( { org.wetator.core.result.AllTests.class, //
+@SuiteClasses({ org.wetator.core.result.AllTests.class, //
     org.wetator.core.searchpattern.AllTests.class, //
-    org.wetator.core.variable.AllTests.class, //
-    CommandTest.class })
+    CommandTest.class, //
+    VariableTest.class })
 public final class AllTests {
 
   /**
