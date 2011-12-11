13b43acd97927e9cef5e268b38a5f44f9e5d3c93
rbri <rbri@rbri.de>
2011-12-11 13:39:23 +0000

    #86; support finding of checkboxes by title attribute

---
+++ src/org/wetator/backend/htmlunit/matcher/ByTitleAttributeMatcher.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2008-2011 wetator.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package org.wetator.backend.htmlunit.matcher;
+
+import org.wetator.backend.WeightedControlList.FoundType;
+import org.wetator.backend.htmlunit.util.FindSpot;
+import org.wetator.backend.htmlunit.util.HtmlPageIndex;
+import org.wetator.core.searchpattern.SearchPattern;
+
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
+
+/**
+ * This matcher checks if the attribute 'title' of the given element matches the criteria.
+ * 
+ * @author rbri
+ */
+public class ByTitleAttributeMatcher extends AbstractByAttributeMatcher {
+
+  /**
+   * The constructor.<br/>
+   * Creates a new matcher with the given criteria.
+   * 
+   * @param aHtmlPageIndex the {@link HtmlPageIndex} of the page the match is based on
+   * @param aPathSearchPattern the {@link SearchPattern} describing the path to the element
+   * @param aPathSpot the {@link FindSpot} the path was found first
+   * @param aSearchPattern the {@link SearchPattern} describing the element
+   */
+  public ByTitleAttributeMatcher(final HtmlPageIndex aHtmlPageIndex, final SearchPattern aPathSearchPattern,
+      final FindSpot aPathSpot, final SearchPattern aSearchPattern) {
+    super(aHtmlPageIndex, aPathSearchPattern, aPathSpot, aSearchPattern, FoundType.BY_TITLE_ATTRIBUTE);
+  }
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see org.wetator.backend.htmlunit.matcher.AbstractByAttributeMatcher#getAttributeValue(com.gargoylesoftware.htmlunit.html.HtmlElement)
+   */
+  @Override
+  protected String getAttributeValue(final HtmlElement aHtmlElement) {
+    return aHtmlElement.getAttribute("title");
+  }
+}
