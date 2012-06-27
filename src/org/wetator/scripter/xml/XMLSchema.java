29c0458e3a33b8ef6c1b0b4686ce97cbee8a295c
fdanek <frank.danek@gmail.com>
2012-06-27 19:36:36 +0000

    % copyright date

--- src/org/wetator/scripter/xml/XMLSchema.java
+++ src/org/wetator/scripter/xml/XMLSchema.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2011 wetator.org
+ * Copyright (c) 2008-2012 wetator.org
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
