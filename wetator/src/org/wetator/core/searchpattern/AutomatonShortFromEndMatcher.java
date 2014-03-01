3e921e58da7b3d36b273f08ae4944af42b7f1fc4
rbri <rbri@localhost>
2014-03-01 09:40:53 +0000

    - code duplicates

--- wetator/src/org/wetator/core/searchpattern/AutomatonShortFromEndMatcher.java
+++ wetator/src/org/wetator/core/searchpattern/AutomatonShortFromEndMatcher.java
@@ -16,20 +16,12 @@
 
 package org.wetator.core.searchpattern;
 
-import java.util.regex.MatchResult;
-
 import dk.brics.automaton.RunAutomaton;
 
 /**
  * @author rbri
  */
-public final class AutomatonShortFromEndMatcher implements MatchResult {
-
-  private final RunAutomaton automaton;
-  private final CharSequence chars;
-
-  private int matchStart = -1;
-  private int matchEnd = -1;
+public final class AutomatonShortFromEndMatcher extends AbstractAutomatonMatcher {
 
   /**
    * Constructor.
@@ -38,8 +30,7 @@ public final class AutomatonShortFromEndMatcher implements MatchResult {
    * @param anAutomaton the automaton to set
    */
   public AutomatonShortFromEndMatcher(final CharSequence aCharSequence, final RunAutomaton anAutomaton) {
-    chars = aCharSequence;
-    automaton = anAutomaton;
+    super(aCharSequence, anAutomaton);
   }
 
   /**
@@ -94,138 +85,5 @@ public final class AutomatonShortFromEndMatcher implements MatchResult {
 
     setMatch(-2, -2);
     return false;
-  }
-
-  private void setMatch(final int aMatchStart, final int aMatchEnd) throws IllegalArgumentException {
-    if (aMatchStart > aMatchEnd) {
-      throw new IllegalArgumentException("Start must be less than or equal to end: " + aMatchStart + ", " + aMatchEnd);
-    }
-    matchStart = aMatchStart;
-    matchEnd = aMatchEnd;
-  }
-
-  /**
-   * Returns the offset after the last character matched.
-   * 
-   * @return The offset after the last character matched.
-   * @throws IllegalStateException if there has not been a match attempt or
-   *         if the last attempt yielded no results.
-   */
-  @Override
-  public int end() throws IllegalStateException {
-    matchGood();
-    return matchEnd;
-  }
-
-  /**
-   * Returns the offset after the last character matched of the specified
-   * capturing group. <br />
-   * Note that because the automaton does not support capturing groups the
-   * only valid group is 0 (the entire match).
-   * 
-   * @param aGroup the desired capturing group.
-   * @return The offset after the last character matched of the specified
-   *         capturing group.
-   * @throws IllegalStateException if there has not been a match attempt or
-   *         if the last attempt yielded no results.
-   * @throws IndexOutOfBoundsException if the specified capturing group does
-   *         not exist in the underlying automaton.
-   */
-  @Override
-  public int end(final int aGroup) throws IndexOutOfBoundsException, IllegalStateException {
-    onlyZero(aGroup);
-    return end();
-  }
-
-  /**
-   * Returns the subsequence of the input found by the previous match.
-   * 
-   * @return The subsequence of the input found by the previous match.
-   * @throws IllegalStateException if there has not been a match attempt or
-   *         if the last attempt yielded no results.
-   */
-  @Override
-  public String group() throws IllegalStateException {
-    matchGood();
-    return chars.subSequence(matchStart, matchEnd).toString();
-  }
-
-  /**
-   * Returns the subsequence of the input found by the specified capturing
-   * group during the previous match operation. <br />
-   * Note that because the automaton does not support capturing groups the
-   * only valid group is 0 (the entire match).
-   * 
-   * @param aGroup the desired capturing group.
-   * @return The subsequence of the input found by the specified capturing
-   *         group during the previous match operation the previous match. Or {@code null} if the given group did match.
-   * @throws IllegalStateException if there has not been a match attempt or
-   *         if the last attempt yielded no results.
-   * @throws IndexOutOfBoundsException if the specified capturing group does
-   *         not exist in the underlying automaton.
-   */
-  @Override
-  public String group(final int aGroup) throws IndexOutOfBoundsException, IllegalStateException {
-    onlyZero(aGroup);
-    return group();
-  }
-
-  /**
-   * Returns the number of capturing groups in the underlying automaton. <br />
-   * Note that because the automaton does not support capturing groups this
-   * method will always return 0.
-   * 
-   * @return The number of capturing groups in the underlying automaton.
-   */
-  @Override
-  public int groupCount() {
-    return 0;
-  }
-
-  /**
-   * Returns the offset of the first character matched.
-   * 
-   * @return The offset of the first character matched.
-   * @throws IllegalStateException if there has not been a match attempt or
-   *         if the last attempt yielded no results.
-   */
-  @Override
-  public int start() throws IllegalStateException {
-    matchGood();
-    return matchStart;
-  }
-
-  /**
-   * Returns the offset of the first character matched of the specified
-   * capturing group. <br />
-   * Note that because the automaton does not support capturing groups the
-   * only valid group is 0 (the entire match).
-   * 
-   * @param aGroup the desired capturing group.
-   * @return The offset of the first character matched of the specified
-   *         capturing group.
-   * @throws IllegalStateException if there has not been a match attempt or
-   *         if the last attempt yielded no results.
-   * @throws IndexOutOfBoundsException if the specified capturing group does
-   *         not exist in the underlying automaton.
-   */
-  @Override
-  public int start(final int aGroup) throws IndexOutOfBoundsException, IllegalStateException {
-    onlyZero(aGroup);
-    return start();
-  }
-
-  /** Helper method that requires the group argument to be 0. */
-  private static void onlyZero(final int aGroup) throws IndexOutOfBoundsException {
-    if (aGroup != 0) {
-      throw new IndexOutOfBoundsException("The only group supported is 0.");
-    }
-  }
-
-  /** Helper method to check that the last match attempt was valid. */
-  private void matchGood() throws IllegalStateException {
-    if (matchStart < 0 || matchEnd < 0) {
-      throw new IllegalStateException("There was no available match.");
-    }
   }
 }
