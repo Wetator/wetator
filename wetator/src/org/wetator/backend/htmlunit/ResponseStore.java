fff87a42ad5c9e947eac129b6ea4ec188fcdfdcc
rbri <rbri@localhost>
2014-05-29 10:34:26 +0000

    % revert last refactoring, too complicated

--- wetator/src/org/wetator/backend/htmlunit/ResponseStore.java
+++ wetator/src/org/wetator/backend/htmlunit/ResponseStore.java
@@ -36,7 +36,7 @@ import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.wetator.backend.htmlunit.content.ContentTypeUtil;
+import org.wetator.backend.htmlunit.util.ContentTypeUtil;
 import org.wetator.exception.ResourceException;
 
 import com.gargoylesoftware.htmlunit.Page;
