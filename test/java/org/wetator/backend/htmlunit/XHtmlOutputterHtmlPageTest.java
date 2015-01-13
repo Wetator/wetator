fb6308d8b7392ff7f4ed19ab220196119b725f74
rbri <rbri@rbri.de>
2015-01-13 20:10:22 +0000

    % fix the expected output to be in sync with htmlunit

--- test/java/org/wetator/backend/htmlunit/XHtmlOutputterHtmlPageTest.java
+++ test/java/org/wetator/backend/htmlunit/XHtmlOutputterHtmlPageTest.java
@@ -37,7 +37,7 @@ public class XHtmlOutputterHtmlPageTest {
       + "<html>\n<head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=ISO-8859-1\"></head><body>\n";
   private static final String TRAILING = "\n</body></html>\n";
 
-  private static final String EXPECTED_LEADING = "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?> "
+  private static final String EXPECTED_LEADING = "<?xml version=\"1.0\" encoding=\"UTF-8\"?> "
       + "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\"> "
       + "<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" lang=\"en\"> "
       + "<head> <meta http-equiv=\"Content-Type\" content=\"text/html; charset=ISO-8859-1\"/> </head> <body>";
