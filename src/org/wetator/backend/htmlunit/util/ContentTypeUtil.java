1c1e8cc096e3a23b907ca649410a478735a38b80
rbri <rbri@rbri.de>
2016-01-27 17:14:04 +0000

    % add xlsx support (wip)

--- src/org/wetator/backend/htmlunit/util/ContentTypeUtil.java
+++ src/org/wetator/backend/htmlunit/util/ContentTypeUtil.java
@@ -45,6 +45,7 @@ public final class ContentTypeUtil {
     define(ContentType.XML, "xml", "text/xml");
     define(ContentType.PDF, "pdf", "application/pdf");
     define(ContentType.XLS, "xls", "application/vnd.ms-excel");
+    define(ContentType.XLSX, "xlsx", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
     define(ContentType.TEXT, "csv", "text/csv");
     // RTF
     define(ContentType.RTF, "rtf", "application/rtf", "text/rtf", "text/richtext", "text/enriched");
