061690845e5146e146ec0901a90c84804222e02a
rbri <rbri@localhost>
2016-06-09 07:53:34 +0000

    initial checkin

---
+++ wetator-jsf-ext/src/org/wetator/primefaces/backend/htmlunit/control/HtmlUnitPrimeFacesOption.java
@@ -0,0 +1,157 @@
+/*
+ * Copyright (c) 2008-2016 wetator.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package org.wetator.primefaces.backend.htmlunit.control;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.wetator.backend.control.ISelectable;
+import org.wetator.backend.htmlunit.control.HtmlUnitBaseControl;
+import org.wetator.backend.htmlunit.control.HtmlUnitBaseControl.ForHtmlElement;
+import org.wetator.backend.htmlunit.control.HtmlUnitBaseControl.IdentifiedBy;
+import org.wetator.backend.htmlunit.util.ExceptionUtil;
+import org.wetator.core.WetatorContext;
+import org.wetator.exception.ActionException;
+import org.wetator.exception.BackendException;
+import org.wetator.i18n.Messages;
+import org.wetator.primefaces.backend.htmlunit.control.identifier.HtmlUnitPrimeFacesOptionInSelectIdentifier;
+
+import com.gargoylesoftware.htmlunit.ScriptException;
+import com.gargoylesoftware.htmlunit.html.DomElement;
+import com.gargoylesoftware.htmlunit.html.HtmlDivision;
+import com.gargoylesoftware.htmlunit.html.HtmlOption;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+import net.sourceforge.htmlunit.corejs.javascript.WrappedException;
+
+/**
+ * This is the implementation of the HTML element 'option' (&lt;option&gt;) using HtmlUnit as backend.
+ *
+ * @author rbri
+ */
+@ForHtmlElement(HtmlDivision.class)
+@IdentifiedBy({ HtmlUnitPrimeFacesOptionInSelectIdentifier.class })
+public class HtmlUnitPrimeFacesOption extends HtmlUnitBaseControl<HtmlDivision> implements ISelectable {
+
+  private static final Log LOG = LogFactory.getLog(HtmlUnitPrimeFacesOption.class);
+
+  private final HtmlOption htmlOption;
+
+  /**
+   * The constructor.
+   *
+   * @param anHtmlElement the {@link HtmlDivision} from the backend
+   */
+  public HtmlUnitPrimeFacesOption(final HtmlDivision anHtmlElement, final HtmlOption anOption) {
+    super(anHtmlElement);
+    htmlOption = anOption;
+  }
+
+  /**
+   * {@inheritDoc}
+   *
+   * @see org.wetator.backend.htmlunit.control.HtmlUnitBaseControl#getDescribingText()
+   */
+  @Override
+  public String getDescribingText() {
+    return "Primefaces " + getHtmlElement();
+  }
+
+  /**
+   * {@inheritDoc}
+   *
+   * @see org.wetator.backend.control.ISelectable#select(org.wetator.core.WetatorContext)
+   */
+  @Override
+  public void select(final WetatorContext aWetatorContext) throws ActionException {
+    final HtmlOption tmpHtmlOption = htmlOption;
+
+    if (tmpHtmlOption.isDisabled()) {
+      final String tmpMessage = Messages.getMessage("elementDisabled", new String[] { getDescribingText() });
+      throw new ActionException(tmpMessage);
+    }
+    if (tmpHtmlOption.getEnclosingSelect().isDisabled()) {
+      final String tmpMessage = Messages.getMessage("elementDisabled", new String[] { getDescribingText() });
+      throw new ActionException(tmpMessage);
+    }
+
+    try {
+      if (tmpHtmlOption.isSelected()) {
+        aWetatorContext.informListenersWarn("elementAlreadySelected", new String[] { getDescribingText() });
+      } else {
+        LOG.debug("Select - HtmlUnitOption.click() '" + tmpHtmlOption + "'");
+//        tmpHtmlOption.click();
+        final int tmpPos = tmpHtmlOption.getEnclosingSelect().getOptions().indexOf(tmpHtmlOption);
+
+        String tmpId = tmpHtmlOption.getEnclosingSelect().getId();
+        if (!tmpId.endsWith("_input")) {
+            final String tmpMessage = Messages.getMessage("PrimefacesOption_unsupportedID", new String[] { tmpId });
+            throw new ActionException(tmpMessage);
+        }
+        tmpId = tmpId.substring(0, tmpId.length() - "input".length());
+        tmpId += Integer.toString(tmpPos);
+
+        final HtmlPage tmpPage = tmpHtmlOption.getHtmlPageOrNull();
+        if (tmpPage == null) {
+            final String tmpMessage = Messages.getMessage("PrimefacesOption_pageNull", new String[] {});
+            throw new ActionException(tmpMessage);
+        }
+
+        final DomElement tmpTarget = tmpPage.getElementById(tmpId);
+        if (tmpTarget == null) {
+            final String tmpMessage = Messages.getMessage("PrimefacesOption_pageNull", new String[] { tmpId });
+            throw new ActionException(tmpMessage);
+        }
+
+        tmpTarget.click();
+      }
+
+      // wait for silence
+      waitForImmediateJobs(aWetatorContext);
+    } catch (final ScriptException e) {
+      aWetatorContext.getBrowser().addFailure("javascriptError", new String[] { e.getMessage() }, e);
+    } catch (final WrappedException e) {
+      final Exception tmpScriptException = ExceptionUtil.getScriptExceptionCauseIfPossible(e);
+      aWetatorContext.getBrowser().addFailure("javascriptError", new String[] { tmpScriptException.getMessage() },
+          tmpScriptException);
+    } catch (final BackendException e) {
+      final String tmpMessage = Messages.getMessage("backendError",
+          new String[] { e.getMessage(), getDescribingText() });
+      throw new ActionException(tmpMessage, e);
+    } catch (final Throwable e) {
+      final String tmpMessage = Messages
+          .getMessage("serverError", new String[] { e.getMessage(), getDescribingText() });
+      throw new ActionException(tmpMessage, e);
+    }
+  }
+
+  /**
+   * {@inheritDoc}
+   *
+   * @see org.wetator.backend.control.ISelectable#isSelected(org.wetator.core.WetatorContext)
+   */
+  @Override
+  public boolean isSelected(final WetatorContext aWetatorContext) {
+    final HtmlOption tmpHtmlOption = htmlOption;
+
+    return tmpHtmlOption.isSelected();
+  }
+
+  @Override
+  public String getUniqueSelector() {
+    return getUniqueSelector(getHtmlElement());
+  }
+}
---
+++ wetator-jsf-ext/src/org/wetator/primefaces/backend/htmlunit/control/identifier/HtmlUnitPrimeFacesOptionInSelectIdentifier.java
@@ -0,0 +1,217 @@
+/*
+ * Copyright (c) 2008-2016 wetator.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package org.wetator.primefaces.backend.htmlunit.control.identifier;
+
+import org.apache.commons.lang3.StringUtils;
+import org.wetator.backend.WPath;
+import org.wetator.backend.WeightedControlList;
+import org.wetator.backend.WeightedControlList.FoundType;
+import org.wetator.backend.htmlunit.control.HtmlUnitOption;
+import org.wetator.backend.htmlunit.control.identifier.AbstractHtmlUnitControlIdentifier;
+import org.wetator.core.searchpattern.SearchPattern;
+import org.wetator.primefaces.backend.htmlunit.control.HtmlUnitPrimeFacesOption;
+import org.wetator.util.FindSpot;
+
+import com.gargoylesoftware.htmlunit.html.HtmlDivision;
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
+import com.gargoylesoftware.htmlunit.html.HtmlOption;
+import com.gargoylesoftware.htmlunit.html.HtmlSelect;
+
+/**
+ * The identifier for a {@link HtmlUnitOption} nested inside a select.<br />
+ * It can be identified by:
+ * <ul>
+ * <li>it's text</li>
+ * <li>it's label attribute</li>
+ * <li>it's value attribute</li>
+ * </ul>
+ * The surrounding select can be identified by:
+ * <ul>
+ * <li>the label text before</li>
+ * <li>it's name</li>
+ * <li>it's id</li>
+ * <li>a label</li>
+ * </ul>
+ * 
+ * @author frank.danek
+ */
+public class HtmlUnitPrimeFacesOptionInSelectIdentifier extends AbstractHtmlUnitControlIdentifier {
+
+    /** Found by label text match. */
+    public static final FoundType BY_LABEL_PF = new FoundType("BY_LABEL_PF", FoundType.BY_LABEL, -1);
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see org.wetator.backend.htmlunit.control.identifier.AbstractHtmlUnitControlIdentifier#isHtmlElementSupported(com.gargoylesoftware.htmlunit.html.HtmlElement)
+   */
+  @Override
+  public boolean isHtmlElementSupported(final HtmlElement aHtmlElement) {
+    if (!(aHtmlElement instanceof HtmlSelect)) {
+        return false;
+    };
+    HtmlSelect tmpSelect = (HtmlSelect) aHtmlElement;
+    
+    HtmlElement tmpParent = (HtmlElement) tmpSelect.getParentNode().getParentNode();
+    String tmpClass = tmpParent.getAttribute("class");
+    return tmpClass.contains("ui-selectonemenu");
+  }
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see org.wetator.backend.htmlunit.control.identifier.AbstractHtmlUnitControlIdentifier#identify(WPath,
+   *      com.gargoylesoftware.htmlunit.html.HtmlElement)
+   */
+  @Override
+  public WeightedControlList identify(final WPath aWPath, final HtmlElement aHtmlElement) {
+    if (aWPath.getLastNode() == null) {
+      // TODO implement table coordinate support
+      return new WeightedControlList();
+    }
+
+    final SearchPattern tmpSearchPattern = aWPath.getLastNode().getSearchPattern();
+
+    SearchPattern tmpSearchPatternSelect;
+    SearchPattern tmpPathSearchPatternSelect = null;
+    FindSpot tmpPathSpotSelect = null;
+    if (aWPath.getPathNodes().isEmpty()) {
+      tmpSearchPatternSelect = SearchPattern.compile("");
+    } else {
+      tmpSearchPatternSelect = aWPath.getPathNodes().get(aWPath.getPathNodes().size() - 1).getSearchPattern();
+      if (aWPath.getPathNodes().size() > 1) {
+        tmpPathSearchPatternSelect = SearchPattern.createFromList(aWPath.getPathNodes(),
+            aWPath.getPathNodes().size() - 1);
+        tmpPathSpotSelect = htmlPageIndex.firstOccurence(tmpPathSearchPatternSelect);
+      }
+    }
+
+    // was the path found at all
+    if (tmpPathSpotSelect == FindSpot.NOT_FOUND) {
+      return new WeightedControlList();
+    }
+
+    final WeightedControlList tmpResult = new WeightedControlList();
+    if (aHtmlElement instanceof HtmlSelect) {
+      // has the node the text before
+      final FindSpot tmpNodeSpot = htmlPageIndex.getPosition(aHtmlElement);
+      if (tmpPathSpotSelect == null || tmpPathSpotSelect.getEndPos() <= tmpNodeSpot.getStartPos()) {
+
+        // if the select follows text directly and text matches => choose it
+        int tmpStartPos = 0;
+        if (tmpPathSpotSelect != null) {
+          tmpStartPos = tmpPathSpotSelect.getEndPos();
+        }
+        final String tmpText = htmlPageIndex.getLabelTextBefore(aHtmlElement, tmpStartPos);
+        if (StringUtils.isNotEmpty(tmpText)) {
+          final int tmpCoverage = tmpSearchPatternSelect.noOfSurroundingCharsIn(tmpText);
+          if (tmpCoverage > -1) {
+            final String tmpTextBefore = htmlPageIndex.getTextBefore(aHtmlElement);
+            final int tmpDistance;
+            if (tmpPathSearchPatternSelect != null) {
+              tmpDistance = tmpPathSearchPatternSelect.noOfCharsAfterLastShortestOccurenceIn(tmpTextBefore);
+            } else {
+              tmpDistance = tmpTextBefore.length();
+            }
+            getOption((HtmlSelect) aHtmlElement, tmpSearchPattern, tmpDistance, tmpResult);
+          }
+        }
+
+        // name
+        final String tmpName = aHtmlElement.getAttribute("name");
+        if (StringUtils.isNotEmpty(tmpName) && tmpSearchPatternSelect.matches(tmpName)) {
+          final int tmpCoverage = tmpSearchPatternSelect.noOfSurroundingCharsIn(tmpName);
+          if (tmpCoverage > -1) {
+            final String tmpTextBefore = htmlPageIndex.getTextBefore(aHtmlElement);
+            final int tmpDistance;
+            if (tmpPathSearchPatternSelect != null) {
+              tmpDistance = tmpPathSearchPatternSelect.noOfCharsAfterLastShortestOccurenceIn(tmpTextBefore);
+            } else {
+              tmpDistance = tmpTextBefore.length();
+            }
+            getOption((HtmlSelect) aHtmlElement, tmpSearchPattern, tmpDistance, tmpResult);
+          }
+        }
+
+        // id
+        final String tmpId = aHtmlElement.getId();
+        if (StringUtils.isNotEmpty(tmpId) && tmpSearchPatternSelect.matches(tmpId)) {
+          final int tmpCoverage = tmpSearchPatternSelect.noOfSurroundingCharsIn(tmpId);
+          if (tmpCoverage > -1) {
+            final String tmpTextBefore = htmlPageIndex.getTextBefore(aHtmlElement);
+            final int tmpDistance;
+            if (tmpPathSearchPatternSelect != null) {
+              tmpDistance = tmpPathSearchPatternSelect.noOfCharsAfterLastShortestOccurenceIn(tmpTextBefore);
+            } else {
+              tmpDistance = tmpTextBefore.length();
+            }
+            getOption((HtmlSelect) aHtmlElement, tmpSearchPattern, tmpDistance, tmpResult);
+          }
+        }
+      }
+    }
+    return tmpResult;
+  }
+
+  /**
+   * Searches for nested option of a given select by label, value or text.
+   * 
+   * @param aSelect HtmlSelect which should contain this option
+   * @param aSearchPattern value or label of option
+   * @param aDistance the distance of the control
+   * @param aWeightedControlList the list to add the control to
+   * @return found
+   */
+  protected boolean getOption(final HtmlSelect aSelect, final SearchPattern aSearchPattern, final int aDistance,
+      final WeightedControlList aWeightedControlList) {
+    boolean tmpFound = false;
+    final Iterable<HtmlOption> tmpOptions = aSelect.getOptions();
+    for (final HtmlOption tmpOption : tmpOptions) {
+      String tmpText = htmlPageIndex.getAsText(tmpOption);
+      final int tmpStart = htmlPageIndex.getPosition(tmpOption).getStartPos();
+      if (StringUtils.isNotEmpty(tmpText)) {
+        final int tmpCoverage = aSearchPattern.noOfSurroundingCharsIn(tmpText);
+        if (tmpCoverage > -1) {
+          aWeightedControlList.add(new HtmlUnitPrimeFacesOption((HtmlDivision) aSelect.getParentNode().getParentNode(), tmpOption), BY_LABEL_PF, tmpCoverage,
+              aDistance, tmpStart, htmlPageIndex.getIndex(tmpOption));
+          tmpFound = true;
+        }
+      }
+
+      tmpText = tmpOption.getLabelAttribute();
+      if (StringUtils.isNotEmpty(tmpText)) {
+        final int tmpCoverage = aSearchPattern.noOfSurroundingCharsIn(tmpText);
+        if (tmpCoverage > -1) {
+          aWeightedControlList.add(new HtmlUnitPrimeFacesOption((HtmlDivision) aSelect.getParentNode().getParentNode(), tmpOption), BY_LABEL_PF, tmpCoverage,
+              aDistance, tmpStart, htmlPageIndex.getIndex(tmpOption));
+          tmpFound = true;
+        }
+      }
+
+      tmpText = tmpOption.getValueAttribute();
+      if (StringUtils.isNotEmpty(tmpText)) {
+        final int tmpCoverage = aSearchPattern.noOfSurroundingCharsIn(tmpText);
+        if (tmpCoverage > -1) {
+          aWeightedControlList.add(new HtmlUnitPrimeFacesOption((HtmlDivision) aSelect.getParentNode().getParentNode(), tmpOption), BY_LABEL_PF, tmpCoverage,
+              aDistance, tmpStart, htmlPageIndex.getIndex(tmpOption));
+          tmpFound = true;
+        }
+      }
+    }
+    return tmpFound;
+  }
+}
