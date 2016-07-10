4ffe65b409893dd44ad861c1f44372fa1b199474
rbri <rbri@localhost>
2016-07-10 07:35:29 +0000

    % take care to always use our own class loader

--- wetator/src/org/wetator/commandset/DefaultCommandSet.java
+++ wetator/src/org/wetator/commandset/DefaultCommandSet.java
@@ -856,7 +856,7 @@ public final class DefaultCommandSet extends AbstractCommandSet {
       final StringBuffer tmpMethodLabel = new StringBuffer(tmpMethodName);
 
       try {
-        final Class<?> tmpClass = ClassUtils.getClass(tmpClassName);
+        final Class<?> tmpClass = ClassUtils.getClass(getClass().getClassLoader(), tmpClassName);
         Method tmpMethod = MethodUtils.getMatchingAccessibleMethod(tmpClass, tmpMethodName, tmpParamTypes);
         if (null == tmpMethod) {
           tmpMethod = MethodUtils.getMatchingAccessibleMethod(tmpClass, tmpMethodName, new Class[] { String[].class });
