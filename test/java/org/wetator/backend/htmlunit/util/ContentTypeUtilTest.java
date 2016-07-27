bb4e87534564ff51cfaba8e3071448b0da3b109e
fdanek <frank.danek@gmail.com>
2016-07-27 21:03:46 +0000

    - warnings

--- test/java/org/wetator/backend/htmlunit/util/ContentTypeUtilTest.java
+++ test/java/org/wetator/backend/htmlunit/util/ContentTypeUtilTest.java
@@ -68,46 +68,46 @@ public class ContentTypeUtilTest {
 
   @Test
   public void getContentTypeHtml() {
-    final Page page = mock(HtmlPage.class);
-    final ContentType tmpType = ContentTypeUtil.getContentType(page);
+    final Page tmpPage = mock(HtmlPage.class);
+    final ContentType tmpType = ContentTypeUtil.getContentType(tmpPage);
     Assert.assertEquals(ContentType.HTML, tmpType);
   }
 
   @Test
   public void getContentTypeXHtml() {
-    final Page page = mock(XHtmlPage.class);
-    final ContentType tmpType = ContentTypeUtil.getContentType(page);
+    final Page tmpPage = mock(XHtmlPage.class);
+    final ContentType tmpType = ContentTypeUtil.getContentType(tmpPage);
     Assert.assertEquals(ContentType.HTML, tmpType);
   }
 
   @Test
   public void getContentTypeText() {
-    final Page page = mock(TextPage.class);
-    final ContentType tmpType = ContentTypeUtil.getContentType(page);
+    final Page tmpPage = mock(TextPage.class);
+    final ContentType tmpType = ContentTypeUtil.getContentType(tmpPage);
     Assert.assertEquals(ContentType.TEXT, tmpType);
   }
 
   @Test
   public void getContentTypeFromResponseSupported() {
-    final WebResponse response = mock(WebResponse.class);
-    when(response.getContentType()).thenReturn("text/css");
+    final WebResponse tmpResponse = mock(WebResponse.class);
+    when(tmpResponse.getContentType()).thenReturn("text/css");
 
-    final Page page = mock(Page.class);
-    when(page.getWebResponse()).thenReturn(response);
+    final Page tmpPage = mock(Page.class);
+    when(tmpPage.getWebResponse()).thenReturn(tmpResponse);
 
-    final ContentType tmpType = ContentTypeUtil.getContentType(page);
+    final ContentType tmpType = ContentTypeUtil.getContentType(tmpPage);
     Assert.assertEquals(ContentType.CSS, tmpType);
   }
 
   @Test
   public void getContentTypeFromResponseNotSupported() {
-    final WebResponse response = mock(WebResponse.class);
-    when(response.getContentType()).thenReturn("application/wetator");
+    final WebResponse tmpResponse = mock(WebResponse.class);
+    when(tmpResponse.getContentType()).thenReturn("application/wetator");
 
-    final Page page = mock(Page.class);
-    when(page.getWebResponse()).thenReturn(response);
+    final Page tmpPage = mock(Page.class);
+    when(tmpPage.getWebResponse()).thenReturn(tmpResponse);
 
-    final ContentType tmpType = ContentTypeUtil.getContentType(page);
+    final ContentType tmpType = ContentTypeUtil.getContentType(tmpPage);
     Assert.assertEquals(ContentType.OTHER, tmpType);
   }
 
@@ -122,62 +122,62 @@ public class ContentTypeUtilTest {
 
   @Test
   public void getFileSuffixHtml() {
-    final Page page = mock(HtmlPage.class);
-    final String tmpSuffix = ContentTypeUtil.getFileSuffix(page);
+    final Page tmpPage = mock(HtmlPage.class);
+    final String tmpSuffix = ContentTypeUtil.getFileSuffix(tmpPage);
     Assert.assertEquals("html", tmpSuffix);
   }
 
   @Test
   public void getFileSuffixXHtml() {
-    final Page page = mock(XHtmlPage.class);
-    final String tmpSuffix = ContentTypeUtil.getFileSuffix(page);
+    final Page tmpPage = mock(XHtmlPage.class);
+    final String tmpSuffix = ContentTypeUtil.getFileSuffix(tmpPage);
     Assert.assertEquals("html", tmpSuffix);
   }
 
   @Test
   public void getFileSuffixText() {
-    final Page page = mock(TextPage.class);
-    final String tmpSuffix = ContentTypeUtil.getFileSuffix(page);
+    final Page tmpPage = mock(TextPage.class);
+    final String tmpSuffix = ContentTypeUtil.getFileSuffix(tmpPage);
     Assert.assertEquals("csv", tmpSuffix);
   }
 
   @Test
   public void getFileSuffixFromResponseSupported() {
-    final WebResponse response = mock(WebResponse.class);
-    when(response.getContentType()).thenReturn("text/css");
+    final WebResponse tmpResponse = mock(WebResponse.class);
+    when(tmpResponse.getContentType()).thenReturn("text/css");
 
-    final String tmpSuffix = ContentTypeUtil.getFileSuffix(response);
+    final String tmpSuffix = ContentTypeUtil.getFileSuffix(tmpResponse);
     Assert.assertEquals("css", tmpSuffix);
   }
 
   @Test
   public void getFileSuffixFromResponseNotSupported() {
-    final WebResponse response = mock(WebResponse.class);
-    when(response.getContentType()).thenReturn("application/wetator");
+    final WebResponse tmpResponse = mock(WebResponse.class);
+    when(tmpResponse.getContentType()).thenReturn("application/wetator");
 
-    final String tmpSuffix = ContentTypeUtil.getFileSuffix(response);
+    final String tmpSuffix = ContentTypeUtil.getFileSuffix(tmpResponse);
     Assert.assertEquals("bin", tmpSuffix);
   }
 
   @Test
   public void getFileSuffixFromResponseNotSupportedContentDisposition() {
-    final WebResponse response = mock(WebResponse.class);
-    when(response.getContentType()).thenReturn("application/wetator");
-    when(response.getResponseHeaderValue(anyString())).thenReturn("attachment; filename=quot.pdf;");
+    final WebResponse tmpResponse = mock(WebResponse.class);
+    when(tmpResponse.getContentType()).thenReturn("application/wetator");
+    when(tmpResponse.getResponseHeaderValue(anyString())).thenReturn("attachment; filename=quot.pdf;");
 
-    String tmpSuffix = ContentTypeUtil.getFileSuffix(response);
+    String tmpSuffix = ContentTypeUtil.getFileSuffix(tmpResponse);
     Assert.assertEquals("pdf", tmpSuffix);
 
-    when(response.getResponseHeaderValue(anyString())).thenReturn("attachment; filename=quot.");
-    tmpSuffix = ContentTypeUtil.getFileSuffix(response);
+    when(tmpResponse.getResponseHeaderValue(anyString())).thenReturn("attachment; filename=quot.");
+    tmpSuffix = ContentTypeUtil.getFileSuffix(tmpResponse);
     Assert.assertEquals("bin", tmpSuffix);
 
-    when(response.getResponseHeaderValue(anyString())).thenReturn("attachment; filename=quot");
-    tmpSuffix = ContentTypeUtil.getFileSuffix(response);
+    when(tmpResponse.getResponseHeaderValue(anyString())).thenReturn("attachment; filename=quot");
+    tmpSuffix = ContentTypeUtil.getFileSuffix(tmpResponse);
     Assert.assertEquals("bin", tmpSuffix);
 
-    when(response.getResponseHeaderValue(anyString())).thenReturn("attachment; filename=");
-    tmpSuffix = ContentTypeUtil.getFileSuffix(response);
+    when(tmpResponse.getResponseHeaderValue(anyString())).thenReturn("attachment; filename=");
+    tmpSuffix = ContentTypeUtil.getFileSuffix(tmpResponse);
     Assert.assertEquals("bin", tmpSuffix);
   }
 }
