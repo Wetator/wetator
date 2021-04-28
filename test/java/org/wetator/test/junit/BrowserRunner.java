bcccba0967ddd177b50e7b332fab7ab6ee676e5b
rbri <rbri@rbri.de>
2021-04-28 17:17:31 +0000

    switch to ff78

--- test/java/org/wetator/test/junit/BrowserRunner.java
+++ test/java/org/wetator/test/junit/BrowserRunner.java
@@ -40,7 +40,7 @@ import org.wetator.backend.IBrowser.BrowserType;
  * public class SomeTest implements BrowserTest {
  *
  *   &#064;Test
- *   &#064;Browsers({ Browser.FIREFOX_68 })
+ *   &#064;Browsers({ Browser.Firefox_78 })
  *   public void test() {
  *     // your test method that is run with Firefox 60
  *   }
