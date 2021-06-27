64007af8eb3a2aa17ee9f74e5ba3e88c04ead48e
fdanek <frank.danek@gmail.com>
2021-06-27 19:48:55 +0000

    + missing tests % cleanup

--- test/java/org/wetator/backend/htmlunit/finder/SettableHtmlUnitControlsFinderTest.java
+++ test/java/org/wetator/backend/htmlunit/finder/SettableHtmlUnitControlsFinderTest.java
@@ -20,6 +20,7 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 
@@ -28,6 +29,11 @@ import org.junit.Test;
 import org.wetator.backend.WPath;
 import org.wetator.backend.WeightedControlList;
 import org.wetator.backend.WeightedControlList.Entry;
+import org.wetator.backend.htmlunit.control.identifier.AbstractHtmlUnitControlIdentifier;
+import org.wetator.backend.htmlunit.control.identifier.HtmlUnitInputFileIdentifier;
+import org.wetator.backend.htmlunit.control.identifier.HtmlUnitInputPasswordIdentifier;
+import org.wetator.backend.htmlunit.control.identifier.HtmlUnitInputTextIdentifier;
+import org.wetator.backend.htmlunit.control.identifier.HtmlUnitTextAreaIdentifier;
 import org.wetator.backend.htmlunit.util.HtmlPageIndex;
 import org.wetator.backend.htmlunit.util.PageUtil;
 import org.wetator.core.WetatorConfiguration;
@@ -44,9 +50,6 @@ public class SettableHtmlUnitControlsFinderTest {
 
   protected WetatorConfiguration config;
 
-  /**
-   * Creates a Wetator configuration.
-   */
   @Before
   public void createWetatorConfiguration() {
     final Properties tmpProperties = new Properties();
@@ -56,40 +59,28 @@ public class SettableHtmlUnitControlsFinderTest {
 
   @Test
   public void empty() throws IOException, InvalidInputException {
-    final String tmpHtmlCode = "<html><body>" + "</body></html>";
-    final HtmlPage tmpHtmlPage = PageUtil.constructHtmlPage(tmpHtmlCode);
-    final HtmlPageIndex tmpHtmlPageIndex = new HtmlPageIndex(tmpHtmlPage);
+    // @formatter:off
+    final String tmpHtmlCode = "<html><body>"
+        + "</body></html>";
+    // @formatter:on
 
-    final SettableHtmlUnitControlsFinder tmpFinder = new SettableHtmlUnitControlsFinder(tmpHtmlPageIndex, null);
-    final WeightedControlList tmpFound = tmpFinder.find(new WPath(new SecretString(), config));
+    final List<Entry> tmpEntriesSorted = find(tmpHtmlCode, "");
 
-    assertEquals(0, tmpFound.getEntriesSorted().size());
-  }
-
-  @Test
-  public void hidden() throws IOException, InvalidInputException {
-    final String tmpHtmlCode = "<html><body>" + "<form action='test'>"
-        + "<input id='myId' type='text' value='SetMe' style='visibility: hidden;'>" + "</form>" + "</body></html>";
-    final HtmlPage tmpHtmlPage = PageUtil.constructHtmlPage(tmpHtmlCode);
-    final HtmlPageIndex tmpHtmlPageIndex = new HtmlPageIndex(tmpHtmlPage);
-
-    final SettableHtmlUnitControlsFinder tmpFinder = new SettableHtmlUnitControlsFinder(tmpHtmlPageIndex, null);
-    final WeightedControlList tmpFound = tmpFinder.find(new WPath(new SecretString(), config));
-
-    assertEquals(0, tmpFound.getEntriesSorted().size());
+    assertEquals(0, tmpEntriesSorted.size());
   }
 
   @Test
   public void firstInputFileOnPage() throws IOException, InvalidInputException {
-    final String tmpHtmlCode = "<html><body>" + "<form action='test'>" + "<input id='myId' type='file' value='SetMe'>"
-        + "</form>" + "</body></html>";
-    final HtmlPage tmpHtmlPage = PageUtil.constructHtmlPage(tmpHtmlCode);
-    final HtmlPageIndex tmpHtmlPageIndex = new HtmlPageIndex(tmpHtmlPage);
+    // @formatter:off
+    final String tmpHtmlCode = "<html><body>"
+        + "<form action='test'>"
+        + "<input id='myId' type='file' value='SetMe'>"
+        + "</form>"
+        + "</body></html>";
+    // @formatter:on
 
-    final SettableHtmlUnitControlsFinder tmpFinder = new SettableHtmlUnitControlsFinder(tmpHtmlPageIndex, null);
-    final WeightedControlList tmpFound = tmpFinder.find(new WPath(new SecretString(), config));
+    final List<Entry> tmpEntriesSorted = find(tmpHtmlCode, "");
 
-    final List<Entry> tmpEntriesSorted = tmpFound.getEntriesSorted();
     assertEquals(1, tmpEntriesSorted.size());
     assertEquals(
         "[HtmlFileInput (id='myId')] found by: BY_ID deviation: 0 distance: 0 start: 0 hierarchy: 0>1>3>4>5 index: 5",
@@ -97,16 +88,36 @@ public class SettableHtmlUnitControlsFinderTest {
   }
 
   @Test
+  public void inputFile() throws IOException, InvalidInputException {
+    // @formatter:off
+    final String tmpHtmlCode = "<html><body>"
+        + "<form action='test'>"
+        + "<input id='otherId' type='file' value='SetMe'>"
+        + "<input id='myId' type='file' value='SetMe'>"
+        + "</form>"
+        + "</body></html>";
+    // @formatter:on
+
+    final List<Entry> tmpEntriesSorted = find(tmpHtmlCode, "myId", HtmlUnitInputFileIdentifier.class);
+
+    assertEquals(1, tmpEntriesSorted.size());
+    assertEquals(
+        "[HtmlFileInput (id='myId')] found by: BY_ID deviation: 0 distance: 0 start: 0 hierarchy: 0>1>3>4>6 index: 6",
+        tmpEntriesSorted.get(0).toString());
+  }
+
+  @Test
   public void firstInputPasswordOnPage() throws IOException, InvalidInputException {
-    final String tmpHtmlCode = "<html><body>" + "<form action='test'>"
-        + "<input id='myId' type='password' value='SetMe'>" + "</form>" + "</body></html>";
-    final HtmlPage tmpHtmlPage = PageUtil.constructHtmlPage(tmpHtmlCode);
-    final HtmlPageIndex tmpHtmlPageIndex = new HtmlPageIndex(tmpHtmlPage);
+    // @formatter:off
+    final String tmpHtmlCode = "<html><body>"
+        + "<form action='test'>"
+        + "<input id='myId' type='password' value='SetMe'>"
+        + "</form>"
+        + "</body></html>";
+    // @formatter:on
 
-    final SettableHtmlUnitControlsFinder tmpFinder = new SettableHtmlUnitControlsFinder(tmpHtmlPageIndex, null);
-    final WeightedControlList tmpFound = tmpFinder.find(new WPath(new SecretString(), config));
+    final List<Entry> tmpEntriesSorted = find(tmpHtmlCode, "");
 
-    final List<Entry> tmpEntriesSorted = tmpFound.getEntriesSorted();
     assertEquals(1, tmpEntriesSorted.size());
     assertEquals(
         "[HtmlPasswordInput (id='myId')] found by: BY_ID deviation: 0 distance: 0 start: 0 hierarchy: 0>1>3>4>5 index: 5",
@@ -114,16 +125,36 @@ public class SettableHtmlUnitControlsFinderTest {
   }
 
   @Test
+  public void inputPassword() throws IOException, InvalidInputException {
+    // @formatter:off
+    final String tmpHtmlCode = "<html><body>"
+        + "<form action='test'>"
+        + "<input id='otherId' type='password' value='SetMe'>"
+        + "<input id='myId' type='password' value='SetMe'>"
+        + "</form>"
+        + "</body></html>";
+    // @formatter:on
+
+    final List<Entry> tmpEntriesSorted = find(tmpHtmlCode, "myId", HtmlUnitInputPasswordIdentifier.class);
+
+    assertEquals(1, tmpEntriesSorted.size());
+    assertEquals(
+        "[HtmlPasswordInput (id='myId')] found by: BY_ID deviation: 0 distance: 5 start: 5 hierarchy: 0>1>3>4>6 index: 6",
+        tmpEntriesSorted.get(0).toString());
+  }
+
+  @Test
   public void firstInputTextOnPage() throws IOException, InvalidInputException {
-    final String tmpHtmlCode = "<html><body>" + "<form action='test'>" + "<input id='myId' type='text' value='SetMe'>"
-        + "</form>" + "</body></html>";
-    final HtmlPage tmpHtmlPage = PageUtil.constructHtmlPage(tmpHtmlCode);
-    final HtmlPageIndex tmpHtmlPageIndex = new HtmlPageIndex(tmpHtmlPage);
+    // @formatter:off
+    final String tmpHtmlCode = "<html><body>"
+        + "<form action='test'>"
+        + "<input id='myId' type='text' value='SetMe'>"
+        + "</form>"
+        + "</body></html>";
+    // @formatter:on
 
-    final SettableHtmlUnitControlsFinder tmpFinder = new SettableHtmlUnitControlsFinder(tmpHtmlPageIndex, null);
-    final WeightedControlList tmpFound = tmpFinder.find(new WPath(new SecretString(), config));
+    final List<Entry> tmpEntriesSorted = find(tmpHtmlCode, "");
 
-    final List<Entry> tmpEntriesSorted = tmpFound.getEntriesSorted();
     assertEquals(1, tmpEntriesSorted.size());
     assertEquals(
         "[HtmlTextInput (id='myId')] found by: BY_ID deviation: 0 distance: 0 start: 0 hierarchy: 0>1>3>4>5 index: 5",
@@ -131,19 +162,104 @@ public class SettableHtmlUnitControlsFinderTest {
   }
 
   @Test
+  public void inputText() throws IOException, InvalidInputException {
+    // @formatter:off
+    final String tmpHtmlCode = "<html><body>"
+        + "<form action='test'>"
+        + "<input id='otherId' type='text' value='SetMe'>"
+        + "<input id='myId' type='text' value='SetMe'>"
+        + "</form>"
+        + "</body></html>";
+    // @formatter:on
+
+    final List<Entry> tmpEntriesSorted = find(tmpHtmlCode, "myId", HtmlUnitInputTextIdentifier.class);
+
+    assertEquals(1, tmpEntriesSorted.size());
+    assertEquals(
+        "[HtmlTextInput (id='myId')] found by: BY_ID deviation: 0 distance: 5 start: 5 hierarchy: 0>1>3>4>6 index: 6",
+        tmpEntriesSorted.get(0).toString());
+  }
+
+  @Test
   public void firstTextAreaOnPage() throws IOException, InvalidInputException {
-    final String tmpHtmlCode = "<html><body>" + "<form action='test'>" + "<textarea id='myId'>SetMe</textarea>"
-        + "</form>" + "</body></html>";
-    final HtmlPage tmpHtmlPage = PageUtil.constructHtmlPage(tmpHtmlCode);
-    final HtmlPageIndex tmpHtmlPageIndex = new HtmlPageIndex(tmpHtmlPage);
+    // @formatter:off
+    final String tmpHtmlCode = "<html><body>"
+        + "<form action='test'>"
+        + "<textarea id='myId'>SetMe</textarea>"
+        + "</form>"
+        + "</body></html>";
+    // @formatter:on
 
-    final SettableHtmlUnitControlsFinder tmpFinder = new SettableHtmlUnitControlsFinder(tmpHtmlPageIndex, null);
-    final WeightedControlList tmpFound = tmpFinder.find(new WPath(new SecretString(), config));
+    final List<Entry> tmpEntriesSorted = find(tmpHtmlCode, "");
 
-    final List<Entry> tmpEntriesSorted = tmpFound.getEntriesSorted();
     assertEquals(1, tmpEntriesSorted.size());
     assertEquals(
         "[HtmlTextArea (id='myId')] found by: BY_ID deviation: 0 distance: 0 start: 0 hierarchy: 0>1>3>4>5 index: 5",
         tmpEntriesSorted.get(0).toString());
+  }
+
+  @Test
+  public void textArea() throws IOException, InvalidInputException {
+    // @formatter:off
+    final String tmpHtmlCode = "<html><body>"
+        + "<form action='test'>"
+        + "<textarea id='otherId'>SetMe</textarea>"
+        + "<textarea id='myId'>SetMe</textarea>"
+        + "</form>"
+        + "</body></html>";
+    // @formatter:on
+
+    final List<Entry> tmpEntriesSorted = find(tmpHtmlCode, "myId", HtmlUnitTextAreaIdentifier.class);
+
+    assertEquals(1, tmpEntriesSorted.size());
+    assertEquals(
+        "[HtmlTextArea (id='myId')] found by: BY_ID deviation: 0 distance: 5 start: 5 hierarchy: 0>1>3>4>7 index: 7",
+        tmpEntriesSorted.get(0).toString());
+  }
+
+  @Test
+  public void visibilityHidden() throws IOException, InvalidInputException {
+    // @formatter:off
+    final String tmpHtmlCode = "<html><body>"
+        + "<form action='test'>"
+        + "<input id='myId' type='text' value='SetMe' style='visibility: hidden;'>"
+        + "</form>"
+        + "</body></html>";
+    // @formatter:on
+
+    final List<Entry> tmpEntriesSorted = find(tmpHtmlCode, "");
+
+    assertEquals(0, tmpEntriesSorted.size());
+  }
+
+  @Test
+  public void displayNone() throws IOException, InvalidInputException {
+    // @formatter:off
+    final String tmpHtmlCode = "<html><body>"
+        + "<form action='test'>"
+        + "<input id='myId' type='text' value='SetMe' style='display: none;'>"
+        + "</form>"
+        + "</body></html>";
+    // @formatter:on
+
+    final List<Entry> tmpEntriesSorted = find(tmpHtmlCode, "");
+
+    assertEquals(0, tmpEntriesSorted.size());
+  }
+
+  @SafeVarargs
+  private final List<Entry> find(final String aHtmlCode, final String aWPath,
+      final Class<? extends AbstractHtmlUnitControlIdentifier>... aKnownIdentifiers)
+      throws IOException, InvalidInputException {
+    final HtmlPage tmpHtmlPage = PageUtil.constructHtmlPage(aHtmlCode);
+    final HtmlPageIndex tmpHtmlPageIndex = new HtmlPageIndex(tmpHtmlPage);
+
+    final SettableHtmlUnitControlsFinder tmpFinder = new SettableHtmlUnitControlsFinder(tmpHtmlPageIndex, null);
+    if (aKnownIdentifiers != null) {
+      tmpFinder.addIdentifiers(Arrays.asList(aKnownIdentifiers));
+    }
+    final WeightedControlList tmpFound = tmpFinder.find(new WPath(new SecretString(aWPath), config));
+
+    return tmpFound.getEntriesSorted();
   }
 }
